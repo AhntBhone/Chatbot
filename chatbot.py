74b24eacd816c01a7b1b26fc14974aeaad06748c
AhntBhone <96278114+AhntBhone@users.noreply.github.com>
2022-05-13 12:15:49 +0630

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -19,3 +19,41 @@ def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
     sentence_words = [lemmatizer.lemmatize(word) for word in sentence_word]
     return sentence_words
+
+def bag_of_words(sentences):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for w in sentence_words:
+        for i, word in enumerat(words):
+            if word == w:
+                bag[i] = 1
+    return np.array(bag)
+
+def predict_class(sentence):
+    bow = bag_of_words(sentence)
+    res = model.predict(np.array([bow]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({'intents': classes[r[0]], 'probability': str(r[1])})
+    return return_list
+
+def get_response(intents_list, intents_json):
+    tag = intents_list[0]['intents']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice(i['responses'])
+            break
+    return result
+
+print("Chatbot is now fully functional hopefully")
+
+while True:
+    message = input('')
+    ints = predict_class(message)
+    res = get_response(ints, intents)
+    print(res)
